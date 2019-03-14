d4b66fd3c8a7fe73413515fc59e43c9c511276fa
VM-LXZ15268 <svantje.lilienthal@tib.eu>
2019-03-14 11:36:41 +0100

    fixing duplicate entries

--- deduplicate/deduplicate.php
+++ deduplicate/deduplicate.php
@@ -7,7 +7,7 @@
 */
 
 // variables
-$projects = []; // array for projects
+$metadata = []; // array for metadata
 (isset($argv[1]) && $argv[0]!==null) ? $input = $argv[1] : $input = 'input.csv'; // filename for input from commandline
 (isset($argv[2]) && $argv[1]!==null) ? $output = $argv[2] : $output = 'output.csv'; // filename for output from commandline
 
@@ -16,28 +16,32 @@ $inputfile = fopen($input, 'r'); // metadata in csv
 
 // get rows with fgetcsv and store them in the array
 while (($row = fgetcsv($inputfile)) !== FALSE){
-  array_push($projects, $row);
+  array_push($metadata, $row);
 }
 
-// read rows and compare reach element to a copy of the array
-foreach ($projects as $i => $project) {
-  foreach ($projects as $j => $copy) {
+// merge metadata into new array
+$merged = [];
 
-    // merge projects with identical id (but skip same element)
-    if($project[0] == $copy[0] && $i != $j){
+foreach ($metadata as $entry) {
 
-        // merge all metadata of the dublicates
-        foreach($project as $key => $element){
+  $id = $entry[0];
 
-            // combine elements, if not identical
-            if ($element != $copy[$key]){
-                $projects[$j][$key] = $element.$copy[$key];
-            }
+  // add entry if not existent
+  if(!isset($merged[$id])){
+
+    $merged[$id] = $entry;
+
+  }else{
+
+      // merge all metadata
+      foreach($entry as $key => $element){
+
+        if($element != $merged[$id][$key] && !empty($merged[$id][$key]) && !empty($element)){
+
+            $merged[$id][$key] .= " |".$element;
+
         }
-
-        // remove dublicate entry
-        unset($projects[$i]);
-    }
+      }
   }
 }
 
@@ -45,8 +49,8 @@ foreach ($projects as $i => $project) {
 $outputfile = fopen($output, 'w');
 
 // write reduplicated array to output file
-foreach ($projects as $row) {
-  fputcsv($outputfile, $row);
+foreach ($merged as $row) {
+    fputcsv($outputfile, $row);
 }
 
 // close files
