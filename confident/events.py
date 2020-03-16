e29418221925ceaf12e70e9c752b17c26a0fec68
lilients <svantje.lilienthal@fu-berlin.de>
2020-03-16 16:32:22 +0100

    using with to handle file

--- confident/events.py
+++ confident/events.py
@@ -31,66 +31,63 @@ if len(sys.argv) == 4: filename = sys.argv[3]
 else: filename ='events.csv'
 
 # prepare output file
-csvfile = open(filename, 'w')
-file = csv.DictWriter(csvfile, fieldnames = fieldnames)
-file.writeheader()
+with open(filename, 'w') as csvfile:
+    file = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    file.writeheader()
 
-# get range from user input
-startId = sys.argv[1]
-stopId = sys.argv[2]
+    # get range from user input
+    startId = sys.argv[1]
+    stopId = sys.argv[2]
 
-# go forward or backward
-if startId <= stopId: step = +1
-else: step = -1
+    # go forward or backward
+    if startId <= stopId: step = +1
+    else: step = -1
 
-# get all ids
-for i in range(int(startId), int(stopId), step):
+    # get all ids
+    for i in range(int(startId), int(stopId), step):
 
-    # store the data of this event in a dictionary
-    result = {}
+        # store the data of this event in a dictionary
+        result = {}
 
-    # get html via url
-    url = "http://wikicfp.com/cfp/servlet/event.showcfp?eventid="+str(i)
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html, 'html.parser', from_encoding="windows-1259")
+        # get html via url
+        url = "http://wikicfp.com/cfp/servlet/event.showcfp?eventid="+str(i)
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser', from_encoding="windows-1259")
 
-    # check if event for this id exists (there should be a h2 with some of the metadata)
-    if soup.find('h2'):
+        # check if event for this id exists (there should be a h2 with some of the metadata)
+        if soup.find('h2'):
 
-        # get dublin core metadata
-        for metadata in dcMetadata:
-            result[metadata] = get_dublin_core_data(metadata)
+            # get dublin core metadata
+            for metadata in dcMetadata:
+                result[metadata] = get_dublin_core_data(metadata)
 
-        # get metadata and its labels from spans
-        for span in soup.find('h2').find_all('span'):
-            if span.attrs.get('content') and span.attrs.get('property'):
-                result[span.attrs.get('property').replace('v:', '')] = span.attrs.get('content').encode('utf-8')
+            # get metadata and its labels from spans
+            for span in soup.find('h2').find_all('span'):
+                if span.attrs.get('content') and span.attrs.get('property'):
+                    result[span.attrs.get('property').replace('v:', '')] = span.attrs.get('content').encode('utf-8')
 
-        # get metadata from h5: categories
-        if soup.find('h5'):
-            categories = ''
-            for category in soup.find('h5').find_all('a'):
-                if category.text != 'Categories':
-                    categories += '"'+category.text+'" '
-            result['Categories'] = categories
+            # get metadata from h5: categories
+            if soup.find('h5'):
+                categories = ''
+                for category in soup.find('h5').find_all('a'):
+                    if category.text != 'Categories':
+                        categories += '"'+category.text+'" '
+                result['Categories'] = categories
 
-        # get metadata from td: dates
-        for metadata in tdMetadata:
-            result[metadata] = get_td_data(metadata)
+            # get metadata from td: dates
+            for metadata in tdMetadata:
+                result[metadata] = get_td_data(metadata)
 
-        # get Call For Papers and Related Resources
-        div = soup.findAll('div', {'class': 'cfp'})
-        result['Call For Papers'] = div[0].text.encode('utf-8') # Call For Papers
-        result['Related Resources'] = div[1].encode('utf-8') # Related Resources
+            # get Call For Papers and Related Resources
+            div = soup.findAll('div', {'class': 'cfp'})
+            result['Call For Papers'] = div[0].text.encode('utf-8') # Call For Papers
+            result['Related Resources'] = div[1].encode('utf-8') # Related Resources
 
-        # write this event to the file
-        file.writerow(result)
+            # write this event to the file
+            file.writerow(result)
 
-    # print id to commandline to give feedback on the process
-    print i
-
-# close csv file
-csvfile.close()
+        # print id to commandline to give feedback on the process
+        print i
 
 # finish
 print 'all done - check ', filename ,' for the results'
