a5b94daf3e1767a7134e4869457d77d96f9e3525
lilients <svantje.lilienthal@fu-berlin.de>
2020-06-19 16:07:37 +0200

    adding cursor handling

--- confident/crossref.py
+++ confident/crossref.py
@@ -1,33 +1,51 @@
-# script to read event metadata from
+# script to read event metadata from crossref
 # use: python crossref.py
 
 import json
 import requests
 import csv
 
+# functions
+
+# call crossref api
+def getData(cursor):
+    return requests.get("https://api.crossref.org/types/proceedings/works?rows="+str(rows)+"&cursor="+cursor).json()
+
+# handle and write data to file
+def handleData(response):
+    # prepare output file
+    with open(filename, 'w') as csvfile:
+        file = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        file.writeheader()
+
+        # get all entries
+        for i in range(1, int(rows), 1):
+
+            # read the metadata of one event from response
+            metadata = response['message']['items'][i]['event']
+
+            # add the title and doi of the proceeding
+            # TODO: .replace('\n', '')
+            metadata['proceedings-title'] = ''.join(response['message']['items'][i]['title']).encode('utf-8')
+            metadata['doi'] = response['message']['items'][i]['DOI']
+
+            # TODO: fix encoding
+            [''.join(x).encode('utf-8') for x in metadata ]
+            print metadata
+
+            # write metadata to file
+            file.writerow(metadata)
+
 # variables
-row = 1000
-# datatypes = ['proceedings', 'proceedings-article', 'proceedings-series'] TODO
-filename ='crossref-events-from-proceeding-series.csv'
+rows = 10
+filename = 'crossref-events-from-proceedings.csv'
 fieldnames = ['name', 'start', 'end', 'acronym', 'location', 'number', 'sponsor', 'theme', 'proceedings-title', 'doi']
 
-# prepare output file
-with open(filename, 'w') as csvfile:
-    file = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    file.writeheader()
+# main: get and handle data, go to next page using the cursor
+response = getData('*')
+handleData(response)
+cursor = response['message']['next-cursor']
 
-    # call crossref api
-    response = requests.get("https://api.crossref.org/types/proceedings-series/works?rows="+str(row)).json()
-
-    # get all entries
-    for i in range(1, int(stopId), 1):
-
-        # read the metadata of one event from response
-        metadata = response['message']['items'][i]['event']
-
-        # add the title and doi of the proceeding
-        metadata['proceedings-title'] = response['message']['items'][i]['title']
-        metadata['doi'] = response['message']['items'][i]['DOI']
-
-        # write metadata to file
-        file.writerow(metadata)
+while cursor:
+    response = getData(cursor)
+    handleData(response)
